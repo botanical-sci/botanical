44aed46377591db1165839f1680682d76939ae18
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-12-07 02:54:50 +0330

    login register bug fix for ui

--- apps/home/pages/account/register/index.tsx
+++ apps/home/pages/account/register/index.tsx
@@ -7,9 +7,9 @@ const breadcrumbList = [
 
 const Register = () => {
   return (
-    <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-full flex flex-col justify-center py-12 px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        <h2 className="text-center text-3xl font-extrabold text-gray-900">
           Register
         </h2>
         <div className="flex justify-center">
