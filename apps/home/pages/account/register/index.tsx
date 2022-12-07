b660eaeab50f9ed324a7c27e4fa6e746c11d319a
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-12-07 15:41:53 +0330

    add phone number optional to regiseter form

--- apps/home/pages/account/register/index.tsx
+++ apps/home/pages/account/register/index.tsx
@@ -58,6 +58,22 @@ const Register = () => {
             </div>
             <div>
               <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Phone number <span className="text-xs">(Optional)</span>
+              </label>
+              <div className="mt-1">
+                <input
+                  id="phone"
+                  name="phone"
+                  type="tel"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+            </div>
+            <div>
+              <label
                 htmlFor="email"
                 className="block text-sm font-medium text-gray-700"
               >
