b523d83337020f0b6b316946c4db871aa2e7023e
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-12-11 01:22:26 +0330

    order detail bug fix

--- apps/home/pages/account/orders/[id].tsx
+++ apps/home/pages/account/orders/[id].tsx
@@ -27,6 +27,18 @@ const orders = [
         imageAlt:
           'Moss green canvas compact backpack with double top zipper, zipper front pouch, and matching carry handle and backpack straps.',
       },
+      {
+        id: 1,
+        name: 'Micro Backpack',
+        description:
+          'Are you a minimalist looking for a compact carry option? The Micro Backpack is the perfect size for your essential everyday carry items. Wear it like a backpack or carry it like a satchel for all-day use.',
+        href: '#',
+        price: '$70.00',
+        imageSrc:
+          'https://tailwindui.com/img/ecommerce-images/order-history-page-03-product-01.jpg',
+        imageAlt:
+          'Moss green canvas compact backpack with double top zipper, zipper front pouch, and matching carry handle and backpack straps.',
+      },
       // More products...
     ],
   },
@@ -43,11 +55,11 @@ const OrderDetails = () => {
       <div className="py-16 sm:py-24">
         <h2 className="sr-only">Recent orders</h2>
         <div className="max-w-7xl mx-auto sm:px-2 lg:px-8">
-          <div className="max-w-2xl mx-auto space-y-8 sm:px-4 lg:max-w-4xl lg:px-0">
+          <div className="max-w-2xl mx-auto space-y-8 px-4 lg:max-w-4xl lg:px-0">
             {orders.map((order) => (
               <div
                 key={order.number}
-                className="bg-white border-t border-b border-gray-200 shadow-sm sm:rounded-lg sm:border"
+                className="bg-white border-t border-b border-gray-200 shadow-sm rounded-lg border"
               >
                 <h3 className="sr-only">
                   Order placed on{' '}
@@ -56,110 +68,39 @@ const OrderDetails = () => {
                   </time>
                 </h3>
 
-                <div className="flex items-center p-4 border-b border-gray-200 sm:p-6 sm:grid sm:grid-cols-4 sm:gap-x-6">
-                  <dl className="flex-1 grid grid-cols-2 gap-x-6 text-sm sm:col-span-3 sm:grid-cols-3 lg:col-span-2">
-                    <div>
+                <div className="flex items-center p-4 border-b border-gray-200 sm:p-6">
+                  <dl className="flex-1 flex flex-wrap flex-col md:flex-row justify-between gap-x-6 text-sm divide-y divide-gray-200 space-y-4 md:divide-y-0 md:space-y-0">
+                    <div className="flex justify-between pt-4 md:block md:pt-0">
                       <dt className="font-medium text-gray-900">
                         Order number
                       </dt>
-                      <dd className="mt-1 text-gray-500">{order.number}</dd>
+                      <dd className="sm:mt-1 text-gray-500">{order.number}</dd>
                     </div>
-                    <div className="hidden sm:block">
+                    <div className="flex justify-between pt-4 md:block md:pt-0">
                       <dt className="font-medium text-gray-900">Date placed</dt>
-                      <dd className="mt-1 text-gray-500">
+                      <dd className="sm:mt-1 text-gray-500">
                         <time dateTime={order.createdDatetime}>
                           {order.createdDate}
                         </time>
                       </dd>
                     </div>
-                    <div>
+                    <div className="flex justify-between pt-4 md:block md:pt-0">
+                      <dt className="font-medium text-gray-900">PAYMENT</dt>
+                      <dd className="sm:mt-1">{order.number}</dd>
+                    </div>
+                    <div className="flex justify-between pt-4 md:block md:pt-0">
+                      <dt className="font-medium text-gray-900">FULFILLMENT</dt>
+                      <dd className="sm:mt-1">{order.number}</dd>
+                    </div>
+                    <div className="flex justify-between pt-4 md:block md:pt-0">
                       <dt className="font-medium text-gray-900">
                         Total amount
                       </dt>
-                      <dd className="mt-1 font-medium text-gray-900">
+                      <dd className="sm:mt-1 font-medium text-gray-900">
                         {order.total}
                       </dd>
                     </div>
                   </dl>
-
-                  <Menu
-                    as="div"
-                    className="relative flex justify-end lg:hidden"
-                  >
-                    <div className="flex items-center">
-                      <Menu.Button className="-m-2 p-2 flex items-center text-gray-400 hover:text-gray-500">
-                        <span className="sr-only">
-                          Options for order {order.number}
-                        </span>
-                        <DotsVerticalIcon
-                          className="w-6 h-6"
-                          aria-hidden="true"
-                        />
-                      </Menu.Button>
-                    </div>
-
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="origin-bottom-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <div className="py-1">
-                          <Menu.Item>
-                            {({ active }) => (
-                              <a
-                                href={order.href}
-                                className={classNames(
-                                  active
-                                    ? 'bg-gray-100 text-gray-900'
-                                    : 'text-gray-700',
-                                  'block px-4 py-2 text-sm'
-                                )}
-                              >
-                                View
-                              </a>
-                            )}
-                          </Menu.Item>
-                          <Menu.Item>
-                            {({ active }) => (
-                              <a
-                                href={order.invoiceHref}
-                                className={classNames(
-                                  active
-                                    ? 'bg-gray-100 text-gray-900'
-                                    : 'text-gray-700',
-                                  'block px-4 py-2 text-sm'
-                                )}
-                              >
-                                Invoice
-                              </a>
-                            )}
-                          </Menu.Item>
-                        </div>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-
-                  <div className="hidden lg:col-span-2 lg:flex lg:items-center lg:justify-end lg:space-x-4">
-                    <a
-                      href={order.href}
-                      className="flex items-center justify-center bg-white py-2 px-2.5 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      <span>View Order</span>
-                      <span className="sr-only">{order.number}</span>
-                    </a>
-                    <a
-                      href={order.invoiceHref}
-                      className="flex items-center justify-center bg-white py-2 px-2.5 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      <span>View Invoice</span>
-                      <span className="sr-only">for order {order.number}</span>
-                    </a>
-                  </div>
                 </div>
 
                 {/* Products */}
