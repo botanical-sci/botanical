a65da72e00906a13aa881625ae5c46c0fd2bcf33
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-12-15 01:15:35 +0330

    bug fix

--- apps/home/pages/search/index.tsx
+++ apps/home/pages/search/index.tsx
@@ -48,7 +48,7 @@ const Search: FC = () => {
               disabled={loading}
               onKeyDown={handleKeyDown}
               onChange={(event) => setSearch(event.target.value)}
-              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-12 sm:text-sm border-gray-300 rounded-md"
+              className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
             <div
               className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5 text-gray-400 cursor-pointer"
