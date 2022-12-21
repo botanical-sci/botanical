24da620bae619216558554c5ae28aeeae3e041a4
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-12-22 02:30:52 +0330

    add mobile menu drawer

--- libs/components/src/header/index.tsx
+++ libs/components/src/header/index.tsx
@@ -16,7 +16,7 @@ import {
   IconX,
 } from '@tabler/icons';
 
-import { Dialog, Transition } from '@headlessui/react';
+import { Dialog, Disclosure, Transition } from '@headlessui/react';
 
 import { MenuModel } from '@shopify/models';
 import { useCartStore, useUserStore } from '@shopify/state';
@@ -176,17 +176,71 @@ const Header: FC<Props> = ({ menu }: Props) => {
                 </div>
               </div>
 
-              <ul className="flex flex-col gap-5 ml-4 mb-5 md:hidden lg:hidden">
-                {menu?.map((category) => (
-                  <a
-                    href="/"
-                    key={category.title}
-                    className="text-sm group relative"
-                  >
-                    {category.title}
-                  </a>
-                ))}
-              </ul>
+              <div className="flex flex-col space-y-2 p-2">
+                {menu.map((category) =>
+                  !category.items || !category.items?.length ? (
+                    <Link
+                      key={category.title}
+                      href={
+                        extractHandleFromUrl(category.url, category.type) ?? ''
+                      }
+                    >
+                      <a
+                        onClick={() => setOpen(false)}
+                        className="bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900 group w-full flex items-center pl-7 pr-2 py-2 text-sm font-medium rounded-md"
+                      >
+                        {category.title}
+                      </a>
+                    </Link>
+                  ) : (
+                    <Disclosure
+                      as="div"
+                      key={category.title}
+                      className="space-y-1"
+                    >
+                      {({ open }) => (
+                        <>
+                          <Disclosure.Button className="bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900 group w-full flex items-center pr-2 py-2 text-left text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500">
+                            <svg
+                              className={classNames(
+                                open
+                                  ? 'text-gray-400 rotate-90'
+                                  : 'text-gray-300',
+                                'mr-2 flex-shrink-0 h-5 w-5 transform group-hover:text-gray-400 transition-colors ease-in-out duration-150'
+                              )}
+                              viewBox="0 0 20 20"
+                              aria-hidden="true"
+                            >
+                              <path
+                                d="M6 6L14 10L6 14V6Z"
+                                fill="currentColor"
+                              />
+                            </svg>
+                            {category.title}
+                          </Disclosure.Button>
+                          <Disclosure.Panel className="space-y-1">
+                            {category.items.map((subItem) => (
+                              <Disclosure.Button
+                                key={subItem.title}
+                                as="a"
+                                href={
+                                  extractHandleFromUrl(
+                                    subItem.url,
+                                    category.type
+                                  ) ?? ''
+                                }
+                                className="group w-full flex items-center pl-10 pr-2 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50"
+                              >
+                                {subItem.title}
+                              </Disclosure.Button>
+                            ))}
+                          </Disclosure.Panel>
+                        </>
+                      )}
+                    </Disclosure>
+                  )
+                )}
+              </div>
 
               <div className="border-t border-gray-200 py-6 px-4 space-y-6">
                 {userStore.user ? (
@@ -211,14 +265,20 @@ const Header: FC<Props> = ({ menu }: Props) => {
                   <>
                     <div className="flow-root">
                       <Link href="/account/login">
-                        <a className="-m-2 p-2 block font-medium text-gray-900">
+                        <a
+                          onClick={() => setOpen(false)}
+                          className="-m-2 p-2 block font-medium text-gray-900"
+                        >
                           Sign in
                         </a>
                       </Link>
                     </div>
                     <div className="flow-root">
                       <Link href="/account/register">
-                        <a className="-m-2 p-2 block font-medium text-gray-900">
+                        <a
+                          onClick={() => setOpen(false)}
+                          className="-m-2 p-2 block font-medium text-gray-900"
+                        >
                           Create account
                         </a>
                       </Link>
