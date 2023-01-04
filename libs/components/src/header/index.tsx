51a53e580ddda9faa99e2325d14db86fdf73a0e8
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2023-01-04 22:51:00 +0330

    header over flow bug fix

--- libs/components/src/header/index.tsx
+++ libs/components/src/header/index.tsx
@@ -328,12 +328,12 @@ const Header: FC<Props> = ({ menu }: Props) => {
                   </Link>
                 </div>
 
-                <div className="ml-auto flex items-center">
+                <div className="ml-auto flex items-center mr-4">
                   <ul className="hidden gap-5 ml-7 lg:flex md:flex mr-6">
                     {menu?.map((category) => (
                       <li
                         key={category.resourceId}
-                        className="text-sm group relative cursor-pointer"
+                        className="text-sm group relative cursor-pointer text-dark"
                       >
                         {(!category.items || category.items.length === 0) && (
                           <Link
