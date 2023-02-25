8c523c6d823deb8f4605d94a28cfd63f09ce8787
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2023-02-25 23:55:40 +0330

    add metatags

--- apps/home/pages/_app.tsx
+++ apps/home/pages/_app.tsx
@@ -14,6 +14,7 @@ import { menuQuery } from '@shopify/graphql-queries';
 import { NextPage } from 'next';
 import { useUserStore } from '@shopify/state';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 
 interface Props extends AppProps {
   menuData: MenuBasicModel;
@@ -45,8 +46,31 @@ const ShopifyApp: NextPage<Props> = ({
 
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-Q86DS2ZZZ0"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-Q86DS2ZZZ0');
+            `}
+      </Script>
       <Head>
         <title>Botanical Skin Science</title>
+        {/* google merchant center */}
+        <meta
+          name="google-site-verification"
+          content="BlU2L6JfPdfAPvzdUSsDsN1N7IxKRU_ei1bMYFOXiyM"
+        />
+        {/* pinterest */}
+        <meta
+          name="p:domain_verify"
+          content="fc29d8e04b501b2d38118d744131ccf1"
+        />
       </Head>
       <main>
         {loading && (
