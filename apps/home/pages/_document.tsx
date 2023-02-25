4b7e52be13066d53342576393160f54a8c5e2ff7
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2023-02-25 23:33:49 +0330

    add google tag manager and analytics

--- apps/home/pages/_document.tsx
+++ apps/home/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class BotanicalDocument extends Document {
   render() {
@@ -15,6 +16,20 @@ class BotanicalDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&family=Raleway:wght@100;200;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-Q86DS2ZZZ0"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-Q86DS2ZZZ0');
+            `}
+          </Script>
         </Head>
         <body>
           <Main />
